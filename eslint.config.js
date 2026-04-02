b74bbe7d5e8615e97696d025a861d302b8a48697
Jehan Afwazi <jehan.labs@gmail.com>
2026-04-02 14:19:07 +0700

    feat: setup ESLint with typescript support

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tseslint.parser,
+        },
+    },
+];
