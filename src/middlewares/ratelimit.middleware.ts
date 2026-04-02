0c62cbcad8b44222e02d03907117bbc4d9b7d9f7
Jehan Afwazi <jehan.labs@gmail.com>
2026-04-02 14:43:41 +0700

    fix: resolve string to number type mismatch env variable

--- src/middlewares/ratelimit.middleware.ts
+++ src/middlewares/ratelimit.middleware.ts
@@ -3,7 +3,7 @@ import { ENV } from "../config/env.js";
 
 export const limiter = rateLimit({
     windowMs: 60 * 1000, // 1 menit
-    max: ENV.RATE_LIMIT_MAX, // max 100 request per IP
+    max: Number(ENV.RATE_LIMIT_MAX), // max 100 request per IP
     standardHeaders: true, // kirim header rate limit
     legacyHeaders: false,
     message: {
