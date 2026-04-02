40a39253d4538099a012c07ba58acfcfe275de98
Jehan Afwazi <jehan.labs@gmail.com>
2026-04-02 16:39:32 +0700

    fix(lint): ignore dist folder in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,20 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import globals from "globals";
 
 export default [
+    {
+        ignores: ["dist/**", "node_modules/**", "build/**"]
+    },
     js.configs.recommended,
     ...tseslint.configs.recommended,
     {
-        files: ["**/*.ts"],
+        files: ["**/*.ts", "**/*.js"],
         languageOptions: {
             parser: tseslint.parser,
+            globals: {
+                ...globals.node,
+            },
         },
     },
 ];
